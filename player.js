430dc2c8f40d7cbc76ec94056cf3aa3c40e6b8fc
Axel Hodler <derxor@gmail.com>
2016-08-08 00:14:45 +0200

    init sound player code

---
+++ player.js
@@ -0,0 +1,50 @@
+// taken from https://gist.github.com/revolunet/f194a6b4cbbd10a304f9 and modified
+
+var async = require('async');
+var lame = require('lame');
+var fs = require('fs');
+var Speaker = require('speaker');
+var volume = require("pcm-volume");
+
+var audioOptions = {
+    channels: 2,
+    bitDepth: 16,
+    sampleRate: 44100,
+    mode: lame.STEREO
+};
+
+
+var song = 'rain-and-thunder.mp3';
+
+function playStream(input, options) {
+    var decoder = lame.Decoder();
+    options = options || {};
+    var v = new volume();
+    if (options.volume) {
+        v.setVolume(options.volume);
+    }
+    var speaker = new Speaker(audioOptions);
+    speaker.on('finish', function() {
+        if (options.loop) {
+            console.log('loop');
+            // i want to restart here
+            start();
+        }
+    });
+    function start() {
+        //input.pos = 0;
+        console.dir(input);
+        v.pipe(speaker);
+        decoder.pipe(v);
+        input.pipe(decoder);
+    }
+    start();
+}
+
+var inputStream = fs.createReadStream(song);
+
+
+playStream(inputStream, {
+    volume: 0.5,
+    loop: true
+});
